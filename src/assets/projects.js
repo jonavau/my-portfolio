ebda0563f9da0e46c8ab274d602544d422215934
greskelll <jonattan44@hotmail.fr>
2023-07-09 22:09:21 +0200

    ajout lien img

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -6,6 +6,11 @@ export const projects = [
 		link: 'https://github.com/greskelll/Projet6OC',
 		description:
 			"Dans ce projet j'ai pût mettre en pratique  la manipulation des éléments du DOM avec JavaScript ,récupérer les données utilisateurs dans le JavaScript via des formulaires et gérer les événements utilisateurs avec JavaScript.",
+		pictures: [
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/Nina2.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/Nina3.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/Nina1.PNG',
+		],
 	},
 	{
 		id: '2',
@@ -14,6 +19,11 @@ export const projects = [
 		link: 'https://greskelll.github.io/Projet9OC/',
 		description:
 			"A l'aide d'outils tel que LightHouse et Wave, j'ai réalisé l'audit d'un site et optimiser ses performances, son accessibilité et son SEO",
+		pictures: [
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/sophie2.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/sophie3.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/sophie1.PNG',
+		],
 	},
 	{
 		id: '3',
@@ -22,6 +32,11 @@ export const projects = [
 		link: 'https://github.com/greskelll/p10-debuggage',
 		description:
 			"J'ai ici débuggé un site déjà réalisé grâce aux DevTools et les logs, puis j'ai rédigé un cahier de recette et valider une librairie de test",
+		pictures: [
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/debug2.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/debug3.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/debug1.PNG',
+		],
 	},
 	{
 		id: '4',
@@ -31,7 +46,9 @@ export const projects = [
 		description:
 			"J'ai ici affiché les données du back end sur l'interface via des appels API ,implémenté la gestion des données avec Redux et Redux-persist pour assurer le fonctionnement du front et configuré des routes API pour la communication client / serveur",
 		pictures: [
-			'https://raw.githubusercontent.com/greskelll/my-portfolio/318667cc2414c171e89f915dd084bbc7896ef674/src/assets/project_img/argent_bank_1.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/argent1.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/Argent2.PNG',
+			'https://raw.githubusercontent.com/greskelll/my-portfolio/master/src/assets/project_img/Argent3.PNG',
 		],
 	},
 ];
